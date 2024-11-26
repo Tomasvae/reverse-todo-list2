b110dd7d4d904613e0ebff11fe30f2621c7a3999
Tomasvae <114391271+Tomasvae@users.noreply.github.com>
2024-11-26 17:29:13 +0100

    fusion avec branche my feature

--- backend/app.js
+++ backend/app.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const taskRoutes = require("./routes/tasks");
 const cors = require('cors');
 
-const app = express();
+const app = express(); 
 
 app.use(cors());
 app.use(express.json());
@@ -18,5 +18,7 @@ app.use('/tasks', taskRoutes);
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+    console.log(`Server is running on port ${PORT}`); 
+    console.log(`we added this line to test the changes`);
 });
+
